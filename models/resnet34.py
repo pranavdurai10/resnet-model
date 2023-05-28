071a7b454cb8806565d50aec46b6195db9f716b2
Pranav Durai <pranavdurai10@gmail.com>
2023-05-28 21:01:11 +0530

    Create resnet34.py
    Added ResNet34 implementation.

---
+++ models/resnet34.py
@@ -0,0 +1,92 @@
+'''
+/////////////////////////////////////////////////////
+Code written by Pranav Durai on 27.05.2023 @ 21:00:00
+
+ResNet Model Implementation
+
+Model: ResNet34
+Framework: PyTorch 2.0
+/////////////////////////////////////////////////////
+'''
+
+# Import necessary libraries
+import torch
+import torch.nn as nn
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != self.expansion * out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, self.expansion * out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * out_channels)
+            )
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += self.shortcut(residual)
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet34(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(ResNet34, self).__init__()
+        self.in_channels = 64
+
+        self.conv1 = nn.Conv2d(3, self.in_channels, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(BasicBlock, 64, num_blocks=3, stride=1)  
+        self.layer2 = self._make_layer(BasicBlock, 128, num_blocks=4, stride=2) 
+        self.layer3 = self._make_layer(BasicBlock, 256, num_blocks=6, stride=2) 
+        self.layer4 = self._make_layer(BasicBlock, 512, num_blocks=3, stride=2) 
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * BasicBlock.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels * block.expansion
+        
+        for _ in range(1, num_blocks):
+            layers.append(block(self.in_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
